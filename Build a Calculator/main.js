8db2a42d17a8c80235b08937ab83d7f5c67749fe
Simon Wilson <simon@decentralize.global>
2018-03-01 14:19:24 +1100

    semi colons added to js file

--- Build a Calculator/main.js
+++ Build a Calculator/main.js
@@ -3,26 +3,27 @@ $(document).ready(function() {
 
 $(".AC").on("click", function() {
   console.log("search button pushed");
-}
-$(".CE").on("click", function() {}
-$(".Divide").on("click", function() {}
-$(".Multiply").on("click", function() {}
+  $("#screen").html("AC");
+};
+$(".CE").on("click", function() {};
+$(".Divide").on("click", function() {};
+$(".Multiply").on("click", function() {};
 
-$(".Seven").on("click", function() {}
-$(".Eight").on("click", function() {}
-$(".Nine").on("click", function() {}
-$(".Subtract").on("click", function() {}
+$(".Seven").on("click", function() {};
+$(".Eight").on("click", function() {};
+$(".Nine").on("click", function() {};
+$(".Subtract").on("click", function() {};
 
-$(".Four").on("click", function() {}
-$(".Five").on("click", function() {}
-$(".Six").on("click", function() {}
-$(".Add").on("click", function() {}
+$(".Four").on("click", function() {};
+$(".Five").on("click", function() {};
+$(".Six").on("click", function() {};
+$(".Add").on("click", function() {};
 
-$(".One").on("click", function() {}
-$(".Two").on("click", function() {}
-$(".Three").on("click", function() {}
-$(".Equals").on("click", function() {}
+$(".One").on("click", function() {};
+$(".Two").on("click", function() {};
+$(".Three").on("click", function() {};
+$(".Equals").on("click", function() {};
 
-$(".Zero").on("click", function() {}
-$(".Zero").on("click", function() {}
+$(".Zero").on("click", function() {};
+$(".Zero").on("click", function() {};
 });
