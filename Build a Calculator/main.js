888503b35d9e43dc769e70a429bc441e17c57265
Simon Wilson <simon@decentralize.global>
2018-03-01 14:32:32 +1100

    changed button outputs to screen

--- Build a Calculator/main.js
+++ Build a Calculator/main.js
@@ -7,57 +7,74 @@ $(".AC").on("click", function() {
 })
 $(".CE").on("click", function() {
   console.log("CE button pushed");
+  $("#screen").html("CE");
 })
 $(".Divide").on("click", function() {
   console.log("Divide button pushed");
+  $("#screen").html("Divide");
 })
 $(".Multiply").on("click", function() {
   console.log("Multiply button pushed");
+  $("#screen").html("Multiply");
 })
 
 $(".Seven").on("click", function() {
   console.log("Seven button pushed");
+  $("#screen").html("Seven");
 })
 $(".Eight").on("click", function() {
   console.log("Eight button pushed");
+  $("#screen").html("Eight");
 })
 $(".Nine").on("click", function() {
   console.log("Nine button pushed");
+  $("#screen").html("Nine");
 })
 $(".Subtract").on("click", function() {
   console.log("Subtract button pushed");
+  $("#screen").html("Subtract");
 })
 
 $(".Four").on("click", function() {
   console.log("Four button pushed");
+  $("#screen").html("Four");
 })
 $(".Five").on("click", function() {
   console.log("Five button pushed");
+  $("#screen").html("Five");
 })
 $(".Six").on("click", function() {
   console.log("Six button pushed");
+  $("#screen").html("Six");
 })
 $(".Add").on("click", function() {
   console.log("Add button pushed");
+  $("#screen").html("Add");
 })
 
 $(".One").on("click", function() {
   console.log("One button pushed");
+  $("#screen").html("One");
 })
 $(".Two").on("click", function() {
   console.log("Two button pushed");
+  $("#screen").html("Two");
 })
 $(".Three").on("click", function() {
   console.log("Three button pushed");
+  $("#screen").html("Three");
 })
 $(".Equals").on("click", function() {
   console.log("Equals button pushed");
+  $("#screen").html("Equals");
 })
 
 $(".Zero").on("click", function() {
   console.log("Zero button pushed");
+  $("#screen").html("Zero");
 })
 $(".Period").on("click", function() {
   console.log("Period button pushed");
+  $("#screen").html("Period");
 })
 });
