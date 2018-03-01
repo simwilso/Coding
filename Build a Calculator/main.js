8d18b84d34452104cf8ebe2e43eb934e66e952ca
Simon Wilson <simon@decentralize.global>
2018-03-01 14:06:30 +1100

    test java button for AC

--- Build a Calculator/main.js
+++ Build a Calculator/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
 //javascript
+
+$(".AC").on("click", function() {
+  $("#screen").html("AC");
+}
+$(".CE").on("click", function() {}
+$(".Divide").on("click", function() {}
+$(".Multiply").on("click", function() {}
+
+$(".Seven").on("click", function() {}
+$(".Eight").on("click", function() {}
+$(".Nine").on("click", function() {}
+$(".Subtract").on("click", function() {}
+
+$(".Four").on("click", function() {}
+$(".Five").on("click", function() {}
+$(".Six").on("click", function() {}
+$(".Add").on("click", function() {}
+
+$(".One").on("click", function() {}
+$(".Two").on("click", function() {}
+$(".Three").on("click", function() {}
+$(".Equals").on("click", function() {}
+
+$(".Zero").on("click", function() {}
+$(".Zero").on("click", function() {}
 });
