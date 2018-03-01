4114d92dd8c2b0d84034eb0214e892e8bdce239a
Simon Wilson <simon@decentralize.global>
2018-03-01 14:27:56 +1100

    button console prints and try replace text

--- Build a Calculator/main.js
+++ Build a Calculator/main.js
@@ -2,28 +2,62 @@ $(document).ready(function() {
 //javascript
 
 $(".AC").on("click", function() {
-  console.log("search button pushed");
+  console.log("AC button pushed");
   $("#screen").html("AC");
-};
-$(".CE").on("click", function() {};
-$(".Divide").on("click", function() {};
-$(".Multiply").on("click", function() {};
+})
+$(".CE").on("click", function() {
+  console.log("CE button pushed");
+})
+$(".Divide").on("click", function() {
+  console.log("Divide button pushed");
+})
+$(".Multiply").on("click", function() {
+  console.log("Multiply button pushed");
+})
 
-$(".Seven").on("click", function() {};
-$(".Eight").on("click", function() {};
-$(".Nine").on("click", function() {};
-$(".Subtract").on("click", function() {};
+$(".Seven").on("click", function() {
+  console.log("Seven button pushed");
+})
+$(".Eight").on("click", function() {
+  console.log("Eight button pushed");
+})
+$(".Nine").on("click", function() {
+  console.log("Nine button pushed");
+})
+$(".Subtract").on("click", function() {
+  console.log("Subtract button pushed");
+})
 
-$(".Four").on("click", function() {};
-$(".Five").on("click", function() {};
-$(".Six").on("click", function() {};
-$(".Add").on("click", function() {};
+$(".Four").on("click", function() {
+  console.log("Four button pushed");
+})
+$(".Five").on("click", function() {
+  console.log("Five button pushed");
+})
+$(".Six").on("click", function() {
+  console.log("Six button pushed");
+})
+$(".Add").on("click", function() {
+  console.log("Add button pushed");
+})
 
-$(".One").on("click", function() {};
-$(".Two").on("click", function() {};
-$(".Three").on("click", function() {};
-$(".Equals").on("click", function() {};
+$(".One").on("click", function() {
+  console.log("One button pushed");
+})
+$(".Two").on("click", function() {
+  console.log("Two button pushed");
+})
+$(".Three").on("click", function() {
+  console.log("Three button pushed");
+})
+$(".Equals").on("click", function() {
+  console.log("Equals button pushed");
+})
 
-$(".Zero").on("click", function() {};
-$(".Zero").on("click", function() {};
+$(".Zero").on("click", function() {
+  console.log("Zero button pushed");
+})
+$(".Period").on("click", function() {
+  console.log("Period button pushed");
+})
 });
