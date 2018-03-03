e52332136020370fed52f872bbe3d2b1fa18b66b
Simon Wilson <simon@decentralize.global>
2018-03-03 21:41:28 +1100

    working but need to finish the clear button and then clean up the html

--- Build a Calculator/main.js
+++ Build a Calculator/main.js
@@ -1,11 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function calculator() {
 
 //javascript
 
 //setup the global variables
 var operator = "";
+var indexOfOp = "";
+var stringArr = "";
+var beforeOp = "";
+var afterOp = "";
 var firstArr = [];
-var secondArr = [];
 var result = 0;
 
 //need to push digits entered to an array until we detect an operator at which point start logging second array
@@ -27,38 +30,36 @@ $(":button").click(function() {
     //set operator and then start capturing second if statement to capture second array
     operator = "+";
     firstArr.push($(this).val());
+  } else if ($(this).val() === "Clear") {
+    //clear the form and screen if AC or CE is pressed
+    $("#screen").replaceWith("");
+    calculator();
+    return;
+
   } else if ($(this).val() === "=") {
     //find the operator in the array
+    stringArr = firstArr.join("");
+    indexOfOp = stringArr.search(/[*/+-]/);
     //create an integer of digits before the operator
+    beforeOp = parseInt(stringArr.slice(0, indexOfOp));
     //create an integer of the digits after the operator
+    afterOp = parseInt(stringArr.slice(indexOfOp + 1));
     //do the calcs
-
+    if (operator === "/") {
+      result = beforeOp / afterOp;
+    } else if (operator === "*") {
+      result = beforeOp * afterOp;
+    } else if (operator === "-") {
+      result = beforeOp - afterOp;
+    } else if (operator === "+") {
+      result = beforeOp + afterOp;
+    }
+    console.log(result);
     //display the result
     $("#screen").replaceWith(result);
-
   } else {
     firstArr.push($(this).val());
   }
-
-
-/*
-//then need to join the arrays and complete the function do the calculation WORKS
-var first = firstArr.join("");
-var firstInt = parseInt(first);
-var second = secondArr.join("");
-var secondInt = parseInt(second);
-
-if (operator === "/") {
-  result = firstInt / secondInt;
-} else if (operator === "*") {
-  result = firstInt * secondInt;
-} else if (operator === "-") {
-  result = firstInt - secondInt;
-} else if (operator === "+") {
-  result = firstInt + secondInt;
-}
-*/
-//console.log(firstArr);
 $("#screen").html(firstArr.join(""));
 });
 });
