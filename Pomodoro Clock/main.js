d5bc9ed17ffc7209f9784c62330a5b78056154a0
Simon Wilson <simon@decentralize.global>
2018-03-08 12:07:25 +1100

    all javascript working still need to style html

--- Pomodoro Clock/main.js
+++ Pomodoro Clock/main.js
@@ -3,59 +3,71 @@ $(document).ready(function pomodoro() {
 // JAVASCRIPT
 
 // SETUP GLOBAL VARIABLES
-var breakTimer = 5;
-var sessionTimer = 30;
-
+var breakTimer = 5;  //break timer variable
+var sessionTimer = 30;  //sessiontime variable
+var inSeconds = sessionTimer * 60;  //convert the session time from minutes to seconds
 
 // SETUP FUNCTIONS
 
-//WORKS - countup and countdown the break timer
-  //psuedocode - if - button is pushed reduce TIME... if + is pushed increase time
+//WORKS - this is the function to countdown the clock.
+//WORKS - display break time when timer gets to 0
+function timeIt() {
+  if (inSeconds>0) {
+  inSeconds--;
+  $("#timer").html(convertMins(inSeconds));
+  console.log(convertMins(inSeconds));
+} else {
+  $("#timer").html("Have a break you deserve it!");
+  console.log("Have a break you deserve it!");
+}
+}
+//WORKS - this is the function to show the output in min:sec format
 
+function convertMins(s) {
+ var min = Math.floor(s / 60);
+ var sec = s % 60;
+ return min + ":" + sec;
+}
 
-
-
-//WORKS - countup and countdown the session timer
-  //psuedocode - if - button is pushed reduce TIME... if + is pushed increase time
-
-//start the session timer countdown
-  //psuedocode - if button is pushed begin counting down the session time to zero
-  //psuedocode - when the timer reaches zero sound alarm and change screen color
-
-//reset the session timer countdown
-  //psuedocode - if the button is pushed reset the session time to session time..restart the function.
-
-
+//WORKS - if the minus button is pressed for break time reduce the 'break value' by 1 and show in min:sec format
+//WORKS - don't let the number go below 0
 
 $(":button").click(function capture() {
   if ($(this).val() === "breakMinus" && breakTimer !== 0) {
     breakTimer = breakTimer - 1;
-    $("#breakTime").html(breakTimer);
+    $("#breakTime").html(breakTimer + ":00");
+
+//WORKS - if the plus button is pressed for the break time increase the break time by 1 and show in min:sec format
 
   } else if ($(this).val() === "breakPlus") {
      breakTimer = breakTimer + 1;
-     $("#breakTime").html(breakTimer);
+     $("#breakTime").html(breakTimer + ":00");
+
+//WORKS - if the minus button is pressed for session time reduce the 'break value' by 1 and show in min:sec format
+//WORKS - don't let the number go below 0
 
   } else if ($(this).val() === "sessionMinus" && sessionTimer !== 0) {
     sessionTimer = sessionTimer - 1;
-    $("#timer").html(sessionTimer);
-    $("#sessionTime").html(sessionTimer);
+    $("#timer").html(sessionTimer + ":00");
+    $("#sessionTime").html(sessionTimer + ":00");
+
+//WORKS - if the plus button is pressed for session time increase the 'break value' by 1 and show in min:sec format
 
   } else if ($(this).val() === "sessionPlus") {
      sessionTimer = sessionTimer + 1;
-     $("#timer").html(sessionTimer);
-     $("#sessionTime").html(sessionTimer);
+     $("#timer").html(sessionTimer + ":00");
+     $("#sessionTime").html(sessionTimer + ":00");
+
+//WORKS - when the user presses the start button begin counting down the timer interval from its current value
+
   } else if ($(this).val() === "start") {
-    //countdown 'sessionTimer' from current value to zero updating timer value every second
+      inSeconds = sessionTimer * 60;  //convert the session time from minutes to seconds
+      setInterval(timeIt, 1000);
+
+//WORKS - reset the function to defaults when the reset button is pushed
 
   } else if ($(this).val() === "reset") {
-    //reset the 'sessionTimer' to 30, reset 'timer' to 30, reset 'breakTimer' to 5
-    breakTimer = 5;
-    sessionTimer = 30;
-    $("#timer").html(sessionTimer);
-    $("#sessionTime").html(sessionTimer);
-    $("#breakTime").html(breakTimer);
+    window.location.reload();
   }
-
 });
 });
