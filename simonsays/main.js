95d32740936ee8c3aea44344872464fa522a4f1d
Simon Wilson <simon@decentralize.global>
2018-03-22 10:10:28 +1100

    submitted version

--- simonsays/main.js
+++ simonsays/main.js
@@ -3,25 +3,19 @@ $(document).ready(function simonsays() {
 
 // SETUP GLOBAL VARIABLES
 var colorObj = {0: "green", 1: "red", 2: "yellow", 3: "blue"};
-//var userObj = {1: "#", 2: "#", 3: "#", 4: "#", 5: "#", 6: "#", 7: "#", 8: "#", 9: "#", 10: "#", 11: "#", 12: "#", 13: "#", 14: "#", 15: "#", 16: "#", 17: "#", 18: "#", 19: "#", 20: "#"};
-//var compObj = {1: "#", 2: "#", 3: "#", 4: "#", 5: "#", 6: "#", 7: "#", 8: "#", 9: "#", 10: "#", 11: "#", 12: "#", 13: "#", 14: "#", 15: "#", 16: "#", 17: "#", 18: "#", 19: "#", 20: "#"};
-
 var level = 20;
 var currentLevel = 0;
 var strict = false;
 var error = false;
-var sounds = [
-  "http://www.soundjay.com/button/sounds/button-4.mp3", //green
-  "http://www.soundjay.com/button/sounds/button-09.mp3", //red
-  "http://www.soundjay.com/button/sounds/button-10.mp3", //yellow
-  "http://www.soundjay.com/button/sounds/button-7.mp3" //blue
-];
+var sound = {
+  green: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3", //green
+  red: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3", //red
+  yellow: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3", //yellow
+  blue: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3" //blue
+};
 
 var userArray = [];
 var compArray = [];
-
-
-//PROGRAM
 
 // FUNCTIONS
 
@@ -74,10 +68,9 @@ function checkSeq() {
 //USERS sequence
 function user(input) {
   userArray.push(input);
-  addSound(id);//, color);
+  addSound(input);
   console.log(userArray);
     if(!checkSeq()) {
-      //if playing strict mode reset everything
       if(strict) {
         console.log("strict");
         compArray = [];
@@ -106,78 +99,22 @@ function user(input) {
 
 function computer() {
     $("#count").html(currentLevel);
-    //setInterval(timer, 4000);
     if(!error) {
       var AITurn = colorObj[(Math.random()*3).toFixed()];
       compArray.push(AITurn);
       $("#"+AITurn).css("background-color", "gray");
       console.log(compArray);
-/*      switch(AITurn) {
-        case "0":
-          compArray.push("green");
-          $("#green").css("background-color", "gray");
-          setInterval(timer, 3000);
-          console.log(compArray);
-          break;
-        case "1":
-          compArray.push("red");
-          $("#red").css("background-color", "gray");
-          setInterval(timer, 3000);
-          console.log(compArray);
-          break;
-        case "2":
-          compArray.push("yellow");
-          $("#yellow").css("background-color", "gray");
-          setInterval(timer, 3000);
-          console.log(compArray);
-          break;
-        case "3":
-          compArray.push("blue");
-          $("#blue").css("background-color", "gray");
-          setInterval(timer, 3000);
-          console.log(compArray);
-          break;
-        } */
       }
       if(error && strict) {
         var AITurn = colorObj[(Math.random()*3).toFixed()];
         compArray.push(AITurn);
         $("#"+AITurn).css("background-color", "gray");
-
-  /*      switch(AITurn) {
-          case "0":
-            compArray.push("green");
-            $("#green").css("background-color", "gray");
-            setInterval(timer, 3000);
-            console.log(compArray);
-            break;
-          case "1":
-            compArray.push("red");
-            $("#red").css("background-color", "gray");
-            setInterval(timer, 3000);
-            console.log(compArray);
-            break;
-          case "2":
-            compArray.push("yellow");
-            $("#yellow").css("background-color", "gray");
-            setInterval(timer, 3000);
-            console.log(compArray);
-            break;
-          case "3":
-            compArray.push("blue");
-            $("#blue").css("background-color", "gray");
-            setInterval(timer, 3000);
-            console.log(compArray);
-            break;
-          } */
         }
         var i = 0;
         var myInterval = setInterval(function() {
           id = compArray[i];
           color = $("#"+AITurn).css("background-color", "");
-          //color = color.split(" ")[1];
-          //console.log(id+" "+color);
-          addSound(id);//, color);
+          addSound(input);
           i++;
           if(i == compArray.length) {
             clearInterval(myInterval);
@@ -228,18 +165,17 @@ function disError() {
   }, 500);
   resetGame();
 }
-
-//Play a sound
-function addSound(id, color) {
-  playSound(id)
+//Add the sound class
+function addSound(input, color) {
+  playSound(input)
   setTimeout(function(){
   }, 500);
 }
 
-// play board sound
-function playSound(id) {
-  var sound = new Audio(playSound[id]);
-  sound.play();
+//play the sound
+function playSound(input) {
+  var Newsound = new Audio(sound[input]);
+  Newsound.play();
 }
 
 // BUTTON timer function
