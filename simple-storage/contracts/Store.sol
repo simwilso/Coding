a10da7798d9705a90a73fa4ac57abcc443dc86cb
Simon Wilson <simon@decentralize.global>
2018-04-11 13:05:06 +1000

    debug tutorial completed truffle developer console testing

--- simple-storage/contracts/Store.sol
+++ simple-storage/contracts/Store.sol
@@ -3,9 +3,18 @@ pragma solidity ^0.4.17;
 contract SimpleStorage {
   uint myVariable;
 
-  function set(uint x) public {
-    myVariable = x;
+  event Odd();
+
+  event Even();
+
+function set(uint x) public {
+  myVariable = x;
+  if (x % 2 == 0) {
+    Odd();
+  } else {
+    Even();
   }
+}
 
   function get() constant public returns (uint) {
     return myVariable;
