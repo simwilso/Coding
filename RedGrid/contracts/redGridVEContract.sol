fa7718231e39403c456336a65fa6764f541ad446
Simon Wilson <simon@decentralize.global>
2018-05-03 13:12:34 +1000

    start again base trufflebox

---
+++ RedGrid/contracts/redGridVEContract.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.18;
+
+contract redGridVEContract {
+
+    string tester = "Hello World";
+
+function GetString() returns (string) {
+    return tester;
+}
+
+}
+
+//setup XTL token
+//setup central bank <Generator> wallet
+//setup wallets for various homes
+
+//function to deposit
+//function to withdraw
