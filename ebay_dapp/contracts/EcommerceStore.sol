e6853aa8f098f3695ae6bf3b8ab23b2011b1e50a
Simon Wilson <simon@decentralize.global>
2018-05-15 11:06:39 +1000

    add bidding reveal and some simple auction result getter functions

--- ebay_dapp/contracts/EcommerceStore.sol
+++ ebay_dapp/contracts/EcommerceStore.sol
@@ -23,11 +23,23 @@ contract EcommerceStore {
   uint totalBids;
   ProductStatus status;
   ProductCondition condition;
+  mapping (address => mapping (bytes32 => Bid)) bids;
  }
+
+ //bid struct
+ struct Bid {
+  address bidder;
+  uint productId;
+  uint value;
+  bool revealed;
+}
+//initialize
 
  function EcommerceStore() public {
   productIndex = 0;
  }
+
+//add products to my store
 
  function addProductToStore(string _name, string _category, string _imageLink, string _descLink, uint _auctionStartTime, uint _auctionEndTime, uint _startPrice, uint _productCondition) public {
   require(_auctionStartTime < _auctionEndTime);
@@ -37,9 +49,98 @@ contract EcommerceStore {
   productIdInStore[productIndex] = msg.sender;
  }
 
+//get a specific product from the store
+
  function getProduct(uint _productId) view public returns (uint, string, string, string, string, uint, uint, uint, ProductStatus, ProductCondition) {
   Product memory product = stores[productIdInStore[_productId]][_productId];
   return (product.id, product.name, product.category, product.imageLink, product.descLink, product.auctionStartTime, product.auctionEndTime, product.startPrice, product.status, product.condition);
+ }
+
+//place a bid on an item
+
+ function bid(uint _productId, bytes32 _bid) payable public returns (bool) {
+   Product storage product = stores[productIdInStore[_productId]][_productId];
+   require (now >= product.auctionStartTime);
+   require (now <= product.auctionEndTime);
+   require (msg.value > product.startPrice);
+   require (product.bids[msg.sender][_bid].bidder == 0);
+   product.bids[msg.sender][_bid] = Bid(msg.sender, _productId, msg.value, false);
+   product.totalBids += 1;
+   return true;
+ }
+
+//reveal the bid and check whether I'm the winner
+
+ function revealBid(uint _productId, string _amount, string _secret) public {
+  Product storage product = stores[productIdInStore[_productId]][_productId];
+  require (now > product.auctionEndTime);
+  bytes32 sealedBid = sha3(_amount, _secret);
+
+  Bid memory bidInfo = product.bids[msg.sender][sealedBid];
+  require (bidInfo.bidder > 0);
+  require (bidInfo.revealed == false);
+
+  uint refund;
+
+  uint amount = stringToUint(_amount);
+
+  if(bidInfo.value < amount) {
+   // They didn't send enough amount, they lost
+   refund = bidInfo.value;
+  } else {
+   // If first to reveal set as highest bidder
+   if (address(product.highestBidder) == 0) {
+    product.highestBidder = msg.sender;
+    product.highestBid = amount;
+    product.secondHighestBid = product.startPrice;
+    refund = bidInfo.value - amount;
+   } else {
+    if (amount > product.highestBid) {
+     product.secondHighestBid = product.highestBid;
+     product.highestBidder.transfer(product.highestBid);
+     product.highestBidder = msg.sender;
+     product.highestBid = amount;
+     refund = bidInfo.value - amount;
+    } else if (amount > product.secondHighestBid) {
+     product.secondHighestBid = amount;
+     refund = amount;
+    } else {
+     refund = amount;
+    }
+   }
+  }
+  product.bids[msg.sender][sealedBid].revealed = true;
+
+  if (refund > 0) {
+   msg.sender.transfer(refund);
+  }
+ }
+
+//getter function to reveal the highest bidders info from the website
+
+ function highestBidderInfo(uint _productId) view public returns (address, uint, uint) {
+   Product memory product = stores[productIdInStore[_productId]][_productId];
+   return (product.highestBidder, product.highestBid, product.secondHighestBid);
+ }
+
+//getter function to check how many bids have been placed
+
+ function totalBids(uint _productId) view public returns (uint) {
+   Product memory product = stores[productIdInStore[_productId]][_productId];
+   return product.totalBids;
+ }
+
+//this function is to convert the hash string 'value field' to a number
+
+ function stringToUint(string s) pure private returns (uint) {
+   bytes memory b = bytes(s);
+   uint result = 0;
+   for (uint i = 0; i < b.length; i++) {
+     if (b[i] >= 48 && b[i] <= 57) {
+       result = result * 10 + (uint(b[i]) - 48);
+     }
+   }
+   return result;
  }
 
 }
